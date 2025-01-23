b53645247cf7226232821db3b87996ee91ea0ff7
shuja hyder <shujamalik04@gmail.com>
2025-01-23 16:29:52 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
-import './App.css'
+import { useState } from 'react';
+import axios from 'axios';
+import './App.css';
 
 function App() {
-  const [name, setName] = useState('');
+  const [zipCode, setZipCode] = useState('');
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
 
-  const fetchApiData = async (name) => {
+  const fetchApiData = async (zipCode) => {
     try {
-      const response = await axios.get(`https://api.agify.io?name=${name}`);
+      const response = await axios.get(`https://api.zippopotam.us/us/${zipCode}`);
       console.log("response", response);
       setData(response.data);
     } catch (error) {
@@ -20,26 +20,28 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    fetchApiData(name);
+    fetchApiData(zipCode);
   };
 
   return (
     <>
       <form onSubmit={handleSubmit}>
         <input 
-          type="text" 
-          value={name} 
-          onChange={(e) => setName(e.target.value)} 
-          placeholder="Enter a name to guess the age" 
+          type="number" 
+          value={zipCode} 
+          onChange={(e) => setZipCode(e.target.value)} 
+          placeholder="Enter a zip code" 
         />
-        <button type="submit">Guess Age</button>
+        <button type="submit">Get Location</button>
       </form>
       {error ? (
         <p>Oops! Something went wrong: {error}</p>
       ) : (
         data && (
           <div>
-            <p>The age of "{name}",  is {data.age} years.</p>
+            <p>Country: {data.country}</p>
+            <p>Country Abbreviation: {data['country abbreviation']}</p>
+            <p>Place Name: {data.places[0]['place name']}</p>
           </div>
         )
       )}
